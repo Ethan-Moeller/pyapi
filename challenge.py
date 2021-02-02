40380d583acc1de8686a1d6905b4e98d39e73886
Ethan Moeller <moeller0728@gmail.com>
2021-02-02 18:31:16 +0000

    ZORK knockoff

--- challenge.py
+++ challenge.py
@@ -11,5 +11,15 @@ def main():
     print("eye color: ", datadecode[3]['eyeColor'])
     print("favorite fruit", datadecode[3]['favoriteFruit'])
 
+
+for person in range(len(datadecode)):
+    print("Name: ",datadecode[person]['name'])
+    print("Email: ", datadecode[person]['email'])
+    print("Phone: ", datadecode[person]['phone'])
+    print("Addresss: ", datadecode[person]['address'])
+    for pal in datadecode[person]['friends']:
+        print(datadecode[person]['friends']['name'])
+
+
 if __name__ == "__main__":
     main()
